0a331c6e2bcfdc1436131d8417fd6bf8e4d4c738
JadenSummerville <79168451+JadenSummerville@users.noreply.github.com>
2024-01-22 19:26:02 -0800

    Added side bar didn't work first time

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 const SideBar = () => {
   const [selectedFile, setSelectedFile] = useState(null);
 
-  const files = ['File1.pdf', 'File2.pdf', 'File3.pdf'];
+  const files = ['File1.pdf', 'File2.pdf', 'File3.pdf']; // Place holder
 
   return (
     <div>
@@ -12,7 +12,7 @@ const SideBar = () => {
         {files.map((file, index) => (
           <li key={index} onClick={() => setSelectedFile(file)} style={{
             padding: '8px', 
-            backgroundColor: selectedFile === file ? '#d7d7d7' : 'transparent', // Change background color for selected file
+            backgroundColor: selectedFile === file ? '#d7d7d7' : 'transparent',
           }}>
             {file}
           </li>
