1fddc7e1460b1727f4a7ec15d9d3007275c4706e
annawng <annawang125@gmail.com>
2024-01-24 11:59:14 -0800

    Fix sidebar component and H3 font size

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -1,26 +1,55 @@
-import { useState } from "react";
+import {
+  FiFileText as Text,
+  FiMusic as Audio,
+  FiFilm as Video,
+} from 'react-icons/fi';
 
-const SideBar = () => {
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [hoveredFile, setHoveredFile] = useState(null);
+import H2 from './H2';
 
-  const files = ['File1.pdf', 'File2.pdf', 'File3.pdf']; // Place holder
+type FileType = 'text' | 'audio' | 'video';
 
+interface File {
+  name: string;
+  type: FileType;
+}
+
+const SideBar = ({
+  files,
+  activeIndex,
+  selectFile,
+}: {
+  files: File[];
+  activeIndex: number;
+  selectFile: (index: number) => void;
+}) => {
   return (
     <div>
-      <b>Upload Files</b>
-      <ul style={{ listStyleType: 'none', padding: 0 }}>
+      <H2 className='text-base mb-2 px-4'>Upload Files</H2>
+      <ul className='p-0 list-none'>
         {files.map((file, index) => (
-          <li key={index} onClick={() => setSelectedFile(file)} 
-          onMouseOver={() => setHoveredFile(file)}
-          onMouseOut={() => setHoveredFile(null)}
-          style={{
-            padding: '8px', 
-            backgroundColor: selectedFile === file ? '#d7d7d7' : hoveredFile === file
-            ? '#f0f0f0'
-            : 'transparent'
-          }}>
-            {file}
+          <li
+            key={index}
+            onClick={() => {
+              selectFile(index);
+            }}
+            className={`px-4 py-2 flex items-center gap-2 rounded-r cursor-pointer transition ${
+              activeIndex === index && 'bg-neutral-200 relative'
+            }`}
+          >
+            {activeIndex === index && (
+              <span
+                className='absolute block inset-0 w-1 bg-accent'
+                aria-hidden='true'
+              ></span>
+            )}
+            {file.type === 'text' ? (
+              <Text />
+            ) : file.type === 'audio' ? (
+              <Audio />
+            ) : (
+              <Video />
+            )}
+            {file.name}
           </li>
         ))}
       </ul>
