94951f6482a396ef2cf2f1b0d661ac1c8cd3e3ca
annawng <annawang125@gmail.com>
2024-01-23 17:47:52 -0800

    Fix Warning component

--- client/src/components/Warning.tsx
+++ client/src/components/Warning.tsx
@@ -1,13 +1,12 @@
-const Warning = ({
-  children,
-}: {
-  children: React.ReactNode;
-}) => {
+import { IoMdWarning as Alert } from 'react-icons/io';
+
+const Warning = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex w-96 h-10 pl-4 bg-warning rounded-md items-center">
+    <div className='flex gap-4 px-4 py-3 w-full bg-warning-light rounded-md items-center'>
+      <Alert className='text-warning' size={28} />
       {children}
     </div>
   );
 };
 
-export default Warning
+export default Warning;
