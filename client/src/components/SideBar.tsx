fb65bd50365a40a0ea4b448cbf3db08c8dfa2a6e
JadenSummerville <79168451+JadenSummerville@users.noreply.github.com>
2024-01-24 22:17:52 -0800

    Added passive hover effect

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const SideBar = () => {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [hoveredFile, setHoveredFile] = useState(null);
 
   const files = ['File1.pdf', 'File2.pdf', 'File3.pdf']; // Place holder
 
@@ -10,9 +11,14 @@ const SideBar = () => {
       <b>Upload Files</b>
       <ul style={{ listStyleType: 'none', padding: 0 }}>
         {files.map((file, index) => (
-          <li key={index} onClick={() => setSelectedFile(file)} style={{
+          <li key={index} onClick={() => setSelectedFile(file)} 
+          onMouseOver={() => setHoveredFile(file)}
+          onMouseOut={() => setHoveredFile(null)}
+          style={{
             padding: '8px', 
-            backgroundColor: selectedFile === file ? '#d7d7d7' : 'transparent',
+            backgroundColor: selectedFile === file ? '#d7d7d7' : hoveredFile === file
+            ? '#f0f0f0'
+            : 'transparent'
           }}>
             {file}
           </li>
