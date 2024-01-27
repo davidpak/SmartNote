87aaac4f63e9c9658c27de8c4c627349b2144483
Thao-Nguyen Ho <uwthao07@uw.edu>
2024-01-27 01:36:59 -0800

    Reset options on new select, updated search bar to allow space, fixed some styles

--- client/src/components/DropdownSelect.tsx
+++ client/src/components/DropdownSelect.tsx
@@ -16,19 +16,19 @@ const DropdownSelect = ({
   options: File[];
   refresh: () => void;
 }) => {
-  const [, setSelected] = useState(options[0])
-  const [searchItem, setOptions] = useState('')
+  const [, setSelected] = useState(options[0]);
+  const [searchItem, setOptions] = useState('');
   const placeholder : File = {
-    name: label
-  }
+    name: label,
+  };
 
   // filter out options that match the search string
   const filteredOptions = options.filter((option) => {
     return option.name.toLowerCase().includes(searchItem.toLowerCase())
-  })
+  });
 
   return (
-    <div className='fixed w-1/2 ml-3'>
+    <div>
       <button
         className='flex items-center text-sm text-neutral-450'
         onClick={() => {
@@ -43,11 +43,12 @@ const DropdownSelect = ({
           className={({ open }) => `${
             open ? 'border-2 border-accent' : 'border border-neutral-450'
           } relative w-full py-2 pl-4 text-left rounded-lg shadow`}
+          onClick={() => setOptions('')}
         >
           {({ value }: { value : File }) =>
             <div className='flex justify-between'>
               <div className='flex items-center gap-4'>
-                <span aria-hidden='true' className={`${ value.name === label && 'hidden' }`}>
+                <span aria-hidden='true' className={`'shrink-0' ${ value.name === label && 'hidden' }`}>
                   {value.icon ? value.icon : <LuFile/>}
                 </span>
                 <span className={`${ value.name === label && 'text-neutral-450' }`}>
@@ -70,6 +71,7 @@ const DropdownSelect = ({
             aria-label="Search through your content"
             className='relative w-5.5/6 py-2 pl-4 m-2 border border-neutral-300 shadow rounded-lg focus:outline-none'
             onChange={(e) => setOptions(e.target.value)}
+            onKeyDown={(e) => { if (e.key === ' ') { e.stopPropagation(); } }}
           />
           {filteredOptions.length === 0 ? (
             <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
@@ -94,7 +96,6 @@ const DropdownSelect = ({
         </Listbox.Options>
       </Listbox>
     </div>
-
   );
 };
 
