59d77598a1117f9d83b256c408a075637590634e
annawng <annawang125@gmail.com>
2024-01-28 13:28:39 -0800

    Add accessible label to logo NavLink

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className='navbar bg-base-100 sticky z-10 w-full px-12 py-5 border-b border-b-neutral-300'>
       <div className='flex w-full items-center justify-between'>
-        <NavLink to='/' className='w-40 shrink-0'>
+        <NavLink to='/' className='w-40 shrink-0' aria-label='Home'>
           <img src='/smartnote-logo.svg' alt='' />
         </NavLink>
 
