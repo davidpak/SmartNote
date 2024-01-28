fe99413dfea1caa1d9289757aca54c93c974efc1
annawng <annawang125@gmail.com>
2024-01-27 20:47:39 -0800

    Refactor DropdownSelect, add animation, minor styling fixes

--- client/src/components/DropdownSelect.tsx
+++ client/src/components/DropdownSelect.tsx
@@ -1,8 +1,12 @@
-import { useState } from 'react'
-import { Listbox } from '@headlessui/react'
-import { LuChevronsUpDown, LuRefreshCcw, LuFile } from 'react-icons/lu';
+import { Dispatch, Fragment, SetStateAction, useState } from 'react';
+import { Listbox, Transition } from '@headlessui/react';
+import {
+  LuChevronsUpDown as Chevrons,
+  LuRefreshCcw as Refresh,
+  LuFile as File,
+} from 'react-icons/lu';
 
-interface File {
+export interface File {
   name: string;
   icon?: string;
 }
@@ -11,89 +15,114 @@ const DropdownSelect = ({
   label,
   options,
   refresh,
+  selectPage,
 }: {
   label: string;
   options: File[];
   refresh: () => void;
+  selectPage: Dispatch<SetStateAction<File>>;
 }) => {
-  const [, setSelected] = useState(options[0]);
-  const [searchItem, setOptions] = useState('');
-  const placeholder : File = {
+  const [searchOptions, setSearchOptions] = useState(options);
+  const placeholder: File = {
     name: label,
   };
-
-  // filter out options that match the search string
-  const filteredOptions = options.filter((option) => {
-    return option.name.toLowerCase().includes(searchItem.toLowerCase())
-  });
 
   return (
     <div>
       <button
-        className='flex items-center text-sm text-neutral-450'
+        className='flex items-center text-sm text-neutral-500 mb-1'
         onClick={() => {
           refresh();
         }}
       >
         Refresh
-        <LuRefreshCcw className='ml-1' aria-hidden='true'/>
+        <Refresh className='ml-1' aria-hidden='true' />
       </button>
-      <Listbox defaultValue={placeholder} onChange={setSelected}>
+      <Listbox defaultValue={placeholder} onChange={selectPage}>
         <Listbox.Button
-          className={({ open }) => `${
-            open ? 'border-2 border-accent' : 'border border-neutral-450'
-          } relative w-full py-2 pl-4 text-left rounded-lg shadow`}
-          onClick={() => setOptions('')}
+          className='border border-neutral-450 focus:outline-accent relative w-full py-2 pl-4 text-left rounded-lg shadow'
+          onClick={() => setSearchOptions(options)}
         >
-          {({ value }: { value : File }) =>
+          {({ value }: { value: File }) => (
             <div className='flex justify-between'>
               <div className='flex items-center gap-4'>
-                <span aria-hidden='true' className={`'shrink-0' ${ value.name === label && 'hidden' }`}>
-                  {value.icon ? value.icon : <LuFile/>}
+                <span
+                  aria-hidden='true'
+                  className={`'shrink-0' ${value.name === label && 'hidden'}`}
+                >
+                  {value.icon ?? <File />}
                 </span>
-                <span className={`${ value.name === label && 'text-neutral-450' }`}>
+                <span
+                  className={`${value.name === label && 'text-neutral-450'}`}
+                >
                   {value.name}
                 </span>
               </div>
               <span className='self-center pr-3'>
-                <LuChevronsUpDown
-                  className='text-neutral-450'
-                  aria-hidden='true'
-                />
+                <Chevrons className='text-neutral-450' aria-hidden='true' />
               </span>
             </div>
-          }
-        </Listbox.Button>
-        <Listbox.Options className='flex flex-col absolute max-h-60 w-full overflow-y-scroll rounded-md py-1 shadow-lg'>
-          <input
-            type='text'
-            placeholder='Search...'
-            aria-label="Search through your content"
-            className='relative w-5.5/6 py-2 pl-4 m-2 border border-neutral-300 shadow rounded-lg focus:outline-none'
-            onChange={(e) => setOptions(e.target.value)}
-            onKeyDown={(e) => { if (e.key === ' ') { e.stopPropagation(); } }}
-          />
-          {filteredOptions.length === 0 ? (
-            <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
-              No results found.
-            </div>
-          ) : (
-            filteredOptions.map((option, index) => (
-              <Listbox.Option
-                key={index}
-                value={option}
-                className={({ active }) =>`${
-                  active ? 'bg-accent text-white' : 'text-black'
-                } flex items-center gap-4 select-none py-2 pl-3`}
-              >
-                <span aria-hidden='true' className='shrink-0'>
-                  {option.icon ? option.icon : <LuFile/>}
-                </span>
-                {option.name}
-              </Listbox.Option>
-            ))
           )}
-        </Listbox.Options>
+        </Listbox.Button>
+
+        <Transition
+          as={Fragment}
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
+          enter='transition ease-in duration-100'
+          leave='transition ease-in duration-100'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'
+        >
+          <Listbox.Options className='flex flex-col absolute max-h-60 w-full overflow-y-scroll rounded-md shadow-lg mt-1'>
+            <div className='sticky top-0 w-full p-2 bg-white'>
+              <input
+                type='text'
+                placeholder='Search'
+                aria-label='Search for a page'
+                className='w-5.5/6 py-2 pl-4 border border-neutral-300 shadow rounded-lg w-full focus:outline-accent'
+                onChange={(e) => {
+                  setSearchOptions(
+                    options.filter((option) => {
+                      // Get search results
+                      return option.name
+                        .toLowerCase()
+                        .includes(e.target.value.toLowerCase());
+                    })
+                  );
+                }}
+                onKeyDown={(e) => {
+                  if (e.key === ' ') {
+                    e.stopPropagation();
+                  }
+                }}
+              />
+            </div>
+
+            {searchOptions.length === 0 ? (
+              <div className='cursor-default select-none px-4 py-2 text-gray-700'>
+                No results found.
+              </div>
+            ) : (
+              searchOptions.map((option: File, index: number) => (
+                <Listbox.Option
+                  key={index}
+                  value={option}
+                  className={({ active }) =>
+                    `${
+                      active ? 'bg-accent text-white' : 'text-black'
+                    } flex items-center gap-4 select-none py-2 pl-3`
+                  }
+                >
+                  <span aria-hidden='true' className='shrink-0'>
+                    {option.icon ?? <File />}
+                  </span>
+                  {option.name}
+                </Listbox.Option>
+              ))
+            )}
+          </Listbox.Options>
+        </Transition>
       </Listbox>
     </div>
   );
