6c028ce2e048d7ed7447e3812e36ffa01fcf7be0
Anna Wang <annawang125@gmail.com>
2024-02-04 16:45:25 -0800

    Add dropzone component (#32)
    * Start working on Dropzone and FileListItem components
    
    * Lift dropzone state up to parent
    
    * Fix color contrast and spacing
    
    * Add aria-hidden to icons

---
+++ client/src/components/Dropzone.tsx
@@ -0,0 +1,96 @@
+import { FileRejection, ErrorCode, useDropzone } from 'react-dropzone';
+import { IoCloudUploadOutline as Upload } from 'react-icons/io5';
+
+import FileListItem from './FileListItem';
+
+const Dropzone = ({
+  files,
+  setFiles,
+  errors,
+  setErrors,
+}: {
+  files: File[];
+  setFiles: (files: File[]) => void;
+  errors: (string | null)[];
+  setErrors: (errors: (string | null)[]) => void;
+}) => {
+  const handleDrop = (
+    acceptedFiles: File[],
+    fileRejections: FileRejection[]
+  ) => {
+    const rejectedFiles = fileRejections.map(({ file }) => file);
+
+    const newErrors = fileRejections.map(({ errors }) => {
+      const error = errors[0]; // Get first error for each file
+      if (error.code === ErrorCode.FileInvalidType) {
+        return 'Invalid file type';
+      }
+      return error.message;
+    });
+
+    setFiles([...files, ...acceptedFiles, ...rejectedFiles]);
+    setErrors([...errors, ...acceptedFiles.map(() => null), ...newErrors]);
+  };
+
+  const { getRootProps, getInputProps, isDragActive, open } = useDropzone({
+    accept: {
+      'application/vnd.openxmlformats-officedocument.presentationml.presentation':
+        [], // .pptx
+      'application/pdf': [],
+    },
+    onDrop: handleDrop,
+  });
+
+  const removeFile = (idx: number) => {
+    setFiles(files.filter((_, index) => idx !== index));
+    setErrors(errors.filter((_, index) => idx !== index));
+  };
+
+  return (
+    <>
+      <div
+        {...getRootProps({
+          className:
+            'dropzone px-8 py-12 border border-dashed border-neutral-300 rounded-3xl text-center',
+        })}
+        tabIndex={-1}
+      >
+        <div className='flex flex-col items-center gap-4'>
+          <Upload size={40} className='text-neutral-500' aria-hidden />
+          <div className='text-center'>
+            <label className='font-medium text-lg text-neutral-500'>
+              {isDragActive
+                ? 'Drop file(s) here'
+                : 'Choose a file or drag & drop it here'}
+              <input type='file' className='hidden' {...getInputProps()} />
+            </label>
+            <p className='text-sm text-neutral-450'>PDF and PPTX formats</p>
+          </div>
+
+          <button
+            onClick={open}
+            className='text-sm text-neutral-475 border border-neutral-300 rounded-xl px-4 py-2 hover:bg-neutral-150 transition'
+          >
+            Browse Files
+          </button>
+        </div>
+      </div>
+
+      {files.length > 0 && (
+        <ul className='flex flex-col gap-4'>
+          {files.map((file: File, index: number) => (
+            <li key={index}>
+              <FileListItem
+                file={file}
+                onRemove={() => removeFile(index)}
+                errorMessage={errors[index] ?? undefined}
+              />
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
+
+export default Dropzone;
---
+++ client/src/components/FileListItem.tsx
@@ -0,0 +1,74 @@
+import prettyBytes from 'pretty-bytes';
+import { FiX as Remove } from 'react-icons/fi';
+import { FaCircleCheck as Success } from 'react-icons/fa6';
+import { IoMdWarning as Error } from 'react-icons/io';
+
+export interface File {
+  name: string;
+  size: number; // in bytes
+}
+
+const FileListItem = ({
+  file,
+  errorMessage,
+  onRemove,
+  className,
+}: {
+  file: File;
+  errorMessage?: string;
+  onRemove?: () => void;
+  className?: string;
+}) => {
+  const { name, size } = file;
+  const type = name.split('.').slice(-1)[0];
+
+  return (
+    <div
+      className={`${className ?? ''} flex w-full justify-between items-center`}
+    >
+      <div className='flex items-center gap-5'>
+        <img
+          src={`/${type === 'pdf' || type === 'pptx' ? type : 'default'}.svg`}
+          alt=''
+          className='w-6 drop-shadow-sm'
+        />
+        <div className='flex items-end gap-5'>
+          <div className='flex flex-col'>
+            <p className='font-medium text-neutral-500'>{name}</p>
+            <div className='flex gap-1'>
+              <p className='text-sm text-neutral-450 w-16'>
+                {prettyBytes(size)}
+              </p>
+              <div className='flex items-center gap-2 text-sm text-neutral-500'>
+                {errorMessage ? (
+                  <>
+                    <Error
+                      size={18}
+                      className='text-warning shrink-0'
+                      aria-hidden
+                    />
+                    <p>{errorMessage}</p>
+                  </>
+                ) : (
+                  <>
+                    <Success className='text-success shrink-0' aria-hidden />
+                    <p>Completed</p>
+                  </>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button
+        onClick={onRemove}
+        aria-label={`Remove ${name}`}
+        className='h-fit'
+      >
+        <Remove size={24} className='text-neutral-500' aria-hidden />
+      </button>
+    </div>
+  );
+};
+
+export default FileListItem;
