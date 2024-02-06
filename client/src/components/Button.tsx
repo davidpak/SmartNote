9709310c3025236330185b2acd3607500460af49
annawng <annawang125@gmail.com>
2024-02-05 18:00:08 -0800

    Fix Button prop interface, add tests, update README

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,28 +1,39 @@
 import React from 'react';
 import { IconType } from 'react-icons';
-import '@testing-library/jest-dom';
+import { twMerge } from 'tailwind-merge';
 
 type Variant = 'primary' | 'secondary' | 'tertiary';
 
-interface ButtonType {
+interface ButtonType extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   icon?: IconType;
   variant?: Variant;
+  className?: string;
 }
 
-const Button = ({ children, icon: Icon, variant = 'primary' }: ButtonType) => {
+const Button = ({
+  children,
+  icon: Icon,
+  variant = 'primary',
+  className,
+  ...rest
+}: ButtonType) => {
   return (
     <button
-      className={`font-semibold rounded-lg px-4 py-2 drop-shadow-md transition-all ${
-        variant === 'primary'
-          ? 'bg-accent text-white hover:bg-accent-dark'
-          : variant === 'secondary'
-          ? 'bg-white border border-neutral-400 text-black hover:bg-neutral-200'
-          : 'font-normal drop-shadow-none hover:text-accent'
-      }`}
+      className={twMerge(
+        `rounded-lg px-4 py-2 drop-shadow-md transition-all w-fit ${
+          variant === 'primary'
+            ? 'bg-accent text-white hover:bg-accent-dark font-semibold'
+            : variant === 'secondary'
+            ? 'bg-white border border-neutral-400 text-black hover:bg-neutral-200 font-medium'
+            : 'font-normal drop-shadow-none hover:text-accent'
+        }`,
+        className
+      )}
+      {...rest}
     >
-      <div className='flex items-center gap-1'>
-        {Icon && <Icon aria-hidden='true' />}
+      <div className='flex items-center gap-2'>
+        {Icon && <Icon aria-hidden='true' className='shrink-0' />}
         {children}
       </div>
     </button>
