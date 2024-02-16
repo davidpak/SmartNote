39d9132363e4c29f5d4b4bd538c72c202b7335a1
Thao-Nguyen Ho <uwthao07@uw.edu>
2024-02-15 18:27:59 -0800

    Forgot to do a pr so revert

--- client/src/components/Customization.tsx
+++ client/src/components/Customization.tsx
@@ -1,13 +1,12 @@
-import { IoMdArrowBack as Arrow } from 'react-icons/io';
+import { IoMdArrowBack as Arrow } from "react-icons/io";
 import { twMerge } from 'tailwind-merge';
 
-import H2 from './H2';
-import H3 from './H3';
-import Body from './Body';
-import Button from './Button';
-import Toggle from './Toggle';
-import Slider from './Slider';
-import Carousel from './Carousel';
+import H2 from "./H2";
+import H3 from "./H3";
+import Body from "./Body";
+import Button from "./Button";
+import Toggle from "./Toggle";
+import Slider from "./Slider";
 
 interface CustomizationType extends React.HTMLAttributes<HTMLDivElement> {
   files: string[];
@@ -51,7 +50,7 @@ const Customization = ({
 
   return (
     <div
-      className={twMerge('flex flex-col items-center gap-16', className)}
+      className={twMerge('flex flex-col items-center gap-10', className)}
       {...rest}
     >
       <Button
@@ -62,70 +61,39 @@ const Customization = ({
       >
         Back
       </Button>
-      <section className='flex flex-col items-center gap-8'>
-        <div className='flex flex-col text-center gap-5 max-w-xl'>
-          <H2>Customize Your Notes</H2>
-          <Body>
-            Toggle the different settings to select which content you want to include
-            in your notes and adjust the verbosity level to control its level of detail!
-          </Body>
+      <div className='flex flex-col text-center gap-5 max-w-xl'>
+        <H2>Customize Your Notes</H2>
+        <Body>
+          Toggle the different settings to select which content you want to include
+          in your notes and adjust the verbosity level to control its level of detail!
+        </Body>
+      </div>
+      <div className='flex gap-52'>
+        <div className='flex flex-col gap-3'>
+          <H3>Content to Include:</H3>
+          <Toggle label='General Overview'/>
+          <Toggle label='Key Concepts'/>
+          <Toggle label='Sections Breakdown'/>
+          <Toggle label='Vocab Words'/>
+          <Toggle label='5th Grader Explanation'/>
+          <Toggle label='Conclusion'/>
         </div>
-        <div className='flex gap-52'>
-          <div className='flex flex-col gap-3'>
-            <H3>Content to Include:</H3>
-            <Toggle label='General Overview'/>
-            <Toggle label='Key Concepts'/>
-            <Toggle label='Sections Breakdown'/>
-            <Toggle label='Vocab Words'/>
-            <Toggle label='5th Grader Explanation'/>
-            <Toggle label='Conclusion'/>
-          </div>
-          <div className='flex flex-col gap-3'>
-            <H3>Additional Customization:</H3>
-            <Slider
-              label='Verbosity'
-              levels={['Low', 'Medium', 'High']}
-            />
-          </div>
+        <div className='flex flex-col gap-3'>
+          <H3>Additional Customization:</H3>
+          <Slider
+            label='Verbosity'
+            levels={['Low', 'Medium', 'High']}
+          />
         </div>
-        <Button
-          onClick={() => {
-            generateNotes();
-            next();
-          }}
-        >
-          Generate!
-        </Button>
-      </section>
-      <section className='flex flex-col items-center gap-5'>
-        <H2>Need Examples?</H2>
-        <Carousel
-          imagePaths={[
-            '/general-overview-ex.png',
-            '/key-concepts-ex.png',
-            '/section-breakdown-ex.png',
-            '/vocab-ex.png',
-            '/5th-grader-ex.png',
-            '/conclusion-ex.png'
-          ]}
-          titles={[
-            'General Overview:',
-            'Key Concepts:',
-            'Sections Breakdown:',
-            'Vocab words:',
-            '5th Grader Explanation:',
-            'Conclusion:'
-          ]}
-          descriptions={[
-            'A top-level breakdown that captures a succinct summary of the uploaded file contents.',
-            'Highlight the top concepts present with a brief summary.',
-            'The core content breakdown, the bread and butter of your notes.',
-            'Capture vocabulary words and definitions in a convenient and concise manner.',
-            'An intuitive and easy-to-comprehend summary.',
-            'Finish off your notes with a comprehensive, succinct conclusion.'
-          ]}
-        />
-      </section>
+      </div>
+      <Button
+        onClick={() => {
+          generateNotes();
+          next();
+        }}
+      >
+        Generate!
+      </Button>
     </div>
   );
 };
