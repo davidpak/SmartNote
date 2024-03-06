feb47a79aef20de862d77f93a3f2b24d839e499e
Thao-Nguyen Ho <uwthao07@uw.edu>
2024-03-05 17:19:12 -0800

    Updated export success page to reflect local vs Notion export

--- client/src/components/ExportSuccess.tsx
+++ client/src/components/ExportSuccess.tsx
@@ -9,12 +9,14 @@ interface ExportSuccessType extends React.HTMLAttributes<HTMLDivElement> {
   prev: () => void;
   goHome: () => void;
   notesUrl: string;
+  isNotion: boolean;
 }
 
 const ExportSuccess = ({
   prev,
   goHome,
   notesUrl,
+  isNotion,
   className,
   ...rest
 }: ExportSuccessType) => {
@@ -33,14 +35,23 @@ const ExportSuccess = ({
       </Button>
       <H2>Export Successful!</H2>
       <img src='/export.png' alt='' className='w-64' />
-      <Body>Congrats! Check out your new notes page :&#41;</Body>
+      <Body>
+        {
+          isNotion ?
+          'Congrats! Check out your new notes page :)' :
+          'Congrats! Your notes have been exported :)'
+        }
+      </Body>
       <div className='flex gap-5'>
         <Button onClick={goHome} variant='secondary'>
           Home
         </Button>
-        <a href={notesUrl} target='_blank' tabIndex={-1}>
-          <Button>Go to Notion</Button>
-        </a>
+        {
+          isNotion &&
+          <a href={notesUrl} target='_blank' tabIndex={-1}>
+            <Button>Go to Notion</Button>
+          </a>
+        }
       </div>
     </div>
   );
