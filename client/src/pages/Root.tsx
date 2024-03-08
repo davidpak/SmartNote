579998c8372a0a44f2849f864a5f6451d3b04b21
annawng <annawang125@gmail.com>
2024-03-07 18:00:01 -0800

    Remove unused import

--- client/src/pages/Root.tsx
+++ client/src/pages/Root.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import Navbar from '../components/Navbar';
 import { FilesContextProvider } from '../contexts/FilesContext';
